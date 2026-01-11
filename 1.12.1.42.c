a40bdab96a451ed577674ba6aecc4b528198d401
ImmortalLord-Ava <3332197564@qq.com>
2026-01-12 02:35:45 +0800

    1.12.1.42.c

--- 1.12.1.42.c
+++ 1.12.1.42.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
 int my_strlen(char *s){
-         char *p=s;//设置了一个指针变量，专门用来装s这个数组的开头（大门）的地址，防止越界。
-	 int a=0;//因为是数组里面的值不可以改，所以不能通过指针直接改数组里的值，来得到最终结果。所以用return。
+         char *p=s;//设置了一个指针变量，专门用来装s这个数组的开头（大门）的地址，防止越界。/因为是数组里面的值不可以改，所以不能通过指针直接改数组里的值，来得到最终结果。所以用return。
 	 while(*p!='\0'){
-	       a++;
 	       p++;
 	 }
-	 return a;
-
+	 return (int)(p-s);
 }
 int main(){
          char str[] = "KernelWarrior";
